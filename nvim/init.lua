bbdc458efcde7fe97297f9b49d01a9c8ef3c3535
ArunavDey <arunavdey01@gmail.com>
2022-12-14 13:10:10 +0530

    [neovim] update config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,20 +1,5 @@
-require "user.options"
-require "user.plugins"
-require "user.keybinds"
-require "user.lsp"
-
--- ##### Theme Settings #####
-vim.cmd("colorscheme gruvbox")
-vim.g.airline_powerline_fonts = 1
-vim.g.airline_theme = "gruvbox"
-vim.g.rainbow_active = 1
-
--- ##### Code Formatting #####
-vim.g.neoformat_basic_format_align = 1
-vim.g.neoformat_basic_format_retab = 1
-vim.g.neoformat_basic_format_trim = 1
-
--- ##### Misc. #####
-vim.g.python3_host_prog = "python"
-vim.g.loaded_perl_provider = 0
-vim.g.loaded_ruby_provider = 0
+require("arunav.keybinds")
+require("arunav.plugins")
+require("arunav.cmd")
+require("arunav.lsp")
+require("arunav.settings")
