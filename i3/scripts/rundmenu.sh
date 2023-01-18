5276f101d78761af613da62e07dec655ddbfb2ee
ArunavDey <arunavdey01@gmail.com>
2023-01-18 19:30:49 +0530

    i3: update configs

---
+++ i3/scripts/rundmenu.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+dmenu_run -i -nb "#282828" -nf "#fbf1c7" -sb "#fbf1c7" -sf "#282828" -fn "JetBrainsMono Nerd Font-12" -b
---
+++ i3/scripts/screenshot.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+scrot ~/Pictures/Screenshots/`date +%Y-%m-%d_%H:%M:%S`.png 
