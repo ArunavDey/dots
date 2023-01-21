89e2f62eecfb10b945326955e8697de7def50b56
ArunavDey <arunavdey01@gmail.com>
2023-01-21 12:24:37 +0530

    update.sh: add a script to update configs

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cp -r ~/.config/nvim ~/GitHub/dots/
+cp -r ~/.config/alacritty ~/GitHub/dots/
+cp -r ~/.config/i3 ~/GitHub/dots/
+cp -r ~/.config/polybar ~/GitHub/dots/
+cp -r ~/.config/zathura ~/GitHub/dots/
+cp -r ~/.config/tmux ~/GitHub/dots/
+cp -r ~/.config/picom ~/GitHub/dots/
+cp -r ~/.config/dunst ~/GitHub/dots/
