59a921cb9c97184a8eee83522db958e592e64ec4
Arunav Dey <arunavdey01@gmail.com>
2023-04-11 16:27:03 +0530

    update dots

--- nvim/lua/arunav/autocmd.lua
+++ nvim/lua/arunav/autocmd.lua
@@ -3,8 +3,14 @@
 ---------------------------------
 local vim = vim
 vim.cmd.colorscheme("gruvbox")
+-- vim.cmd.colorscheme("gruvbox-baby")
+-- vim.cmd.colorscheme("gruvbox-material")
+-- vim.cmd.colorscheme("kanagawa")
+-- vim.cmd.colorscheme("sonokai")
+-- vim.cmd.colorscheme("tokyonight")
+-- vim.cmd.colorscheme("zephyr")
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 vim.cmd([[ autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]])
