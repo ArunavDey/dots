c7e84db720c5fd933ef2d07abeff5bfae3f14bc4
Arunav Dey <arunav.dey@protonmail.com>
2024-01-06 20:30:08 +0530

    remove unnecessary files

--- nvim/lua/arunav/custom_commands.lua
+++
@@ -1,5 +0,0 @@
-local vim = vim
-local cmd = vim.cmd
-
--- Define a Neovim command called Projects
-vim.cmd('command! -bang Projects call fzf#run(fzf#wrap({ "source": "find ~/dev/projects -maxdepth 1 -type d -not -path \'*/.git*\'" }, <bang>0))')
