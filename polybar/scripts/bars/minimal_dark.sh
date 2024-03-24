55b26d488013ebfb1b94d52ce9131eacdfd65cbc
ArunavDey <arunavdey01@gmail.com>
2024-03-24 22:50:21 +0530

    remove a few configs

--- polybar/scripts/bars/minimal_dark.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-killall polybar
-
-polybar -q minimal_dark -c $HOME/.config/polybar/bars/minimal_dark.ini
--- polybar/scripts/setup2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-killall polybar
-
-while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
-
-
-polybar -q minimal -c $HOME/.config/polybar/config.ini &
